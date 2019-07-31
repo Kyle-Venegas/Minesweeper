cb64ba2b851c0ac22794eaaff181be3ae86afb0f
Kyle-Venegas <venegas.kyle@gmail.com>
2019-07-30 22:10:50 -0700

    free space reveals adjacent squares

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,8 @@
 import os
 import random
 
-BOARD_SIZE = 8
-MINES = 10
+BOARD_SIZE = 10
+MINES = 15
 LAST_INDEX = BOARD_SIZE - 1
 
 class board:
@@ -25,6 +25,13 @@ class board:
 #       flag = input("flag?(y/n)")
         if bomb.grid[y][x] == 0:
             self.grid[y][x] = str(bomb.numbers(y, x))
+            # for free space
+            if bomb.numbers(y, x) == ' ':
+                self._check_row(y, x)
+                self._check_col(y, x)
+                self._check_slash(y, x)
+                self._check_backslash(y, x)
+
         if bomb.grid[y][x]:
             for y in range(BOARD_SIZE):
                 for x in range(BOARD_SIZE):
@@ -33,8 +40,68 @@ class board:
             return True
 #       if flag == 'y':
 #           self.grid[y][x] = 'f'
-        
 
+    def _check_row(self, y, x):
+        n = 0
+        if x == 0:
+            if bomb.grid[y][x + 1] == 0:
+                self.grid[y][x + 1] = str(bomb.numbers(y, x + 1))
+        if x == LAST_INDEX:
+            if bomb.grid[y][x - 1] == 0:
+                self.grid[y][x - 1] = str(bomb.numbers(y, x - 1))
+        if 1 <= x and x < LAST_INDEX:
+            for i in range(x - 1, x + 2, 2):
+                if bomb.grid[y][i] == 0:
+                    self.grid[y][i] = str(bomb.numbers(y, i))
+
+    def _check_col(self, y, x):
+        n = 0
+        if y == 0:
+            if bomb.grid[y + 1][x] == 0:
+                self.grid[y + 1][x] = str(bomb.numbers(y + 1, x))
+        if y == LAST_INDEX:
+            if bomb.grid[y - 1][x] == 0:
+                self.grid[y - 1][x] = str(bomb.numbers(y - 1, x))
+        if 1 <= y and y < LAST_INDEX:
+            for i in range(y - 1, y + 2, 2):
+                if bomb.grid[i][x] == 0:
+                    self.grid[i][x] = str(bomb.numbers(i, x))
+        return n
+
+    def _check_slash(self, y, x):
+        n = 0
+        if 1 <= y and 1 <= x and y < LAST_INDEX and x < LAST_INDEX:
+            y += 1
+            for i in range(x - 1, x + 2, 2):
+                if bomb.grid[y][i] == 0:
+                    self.grid[y][i] = str(bomb.numbers(y, i))
+                y -= 2
+        # boundary conditions
+        elif (x == 0 and y != 0) or (y == LAST_INDEX and x != LAST_INDEX):
+            if bomb.grid[y - 1][x + 1] == 0:
+                self.grid[y - 1][x + 1] = str(bomb.numbers(y - 1, x + 1))
+        elif (x == LAST_INDEX and y != LAST_INDEX) or (y == 0 and x != 0):
+            if bomb.grid[y + 1][x - 1] == 0:
+                self.grid[y + 1][x - 1] = str(bomb.numbers(y + 1, x - 1))
+        return n
+
+    def _check_backslash(self, y, x):
+        n = 0
+        if 1 <= y and 1 <= x and y < LAST_INDEX and x < LAST_INDEX:
+            y -= 1
+            for i in range(x - 1, x + 2, 2):
+                if bomb.grid[y][i] == 0:
+                    self.grid[y][i] = str(bomb.numbers(y, i))
+                y += 2
+        # boundary conditions
+        elif (x == 0 and y != LAST_INDEX) or (y == 0 and x != LAST_INDEX):
+            if bomb.grid[y + 1][x + 1] == 0:
+                self.grid[y + 1][x + 1] = str(bomb.numbers(y + 1, x + 1))
+        elif (x == LAST_INDEX and y != 0) or (y == LAST_INDEX and x != 0):
+            if bomb.grid[y - 1][x - 1] == 1:
+                self.grid[y + 1][x - 1] = str(bomb.numbers(y + 1, x - 1))
+        return n
+            
 class mines:
 
     def __init__(self):
@@ -53,10 +120,10 @@ class mines:
 
     def draw(self):
         columns = "  ".join([str(i) for i in range(BOARD_SIZE)])
-        print("\n      ", columns)
+        print("\n    ", columns)
         y = 0
         for row in self.grid:
-            print('   ', y, row)
+            print(' ', y, row)
             y += 1
 
     def numbers(self, y, x):
@@ -68,7 +135,7 @@ class mines:
         if n == 0:
             return ' '
         return n
-                    
+
     def _check_row(self, y, x):
         n = 0
         if x == 0:
